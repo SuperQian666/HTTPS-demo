8e610c9f554f52bde3fa3b236aaeb1a088321b3b
qianyu <qianyu@xiaoduotech.com>
2022-10-03 17:34:15 +0800

    go build

--- server.go
+++ server.go
@@ -31,7 +31,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	if err := http.ListenAndServeTLS("localhost:443", "server.pem", "server.key", nil); err != nil {
+	if err := http.ListenAndServeTLS("localhost:80", "server.pem", "server.key", nil); err != nil {
 		fmt.Println(err)
 	}
 }
